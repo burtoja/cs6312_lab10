5178b26092d6085a0430f59f5e74e0a37c518dd6
J. Allen Burton <jburton6@my.westga.edu>
2020-03-21 20:30:18 -0400

    SortTimer class written

---
+++ src/edu/westga/cs6312/sorting/model/SortTimer.java
@@ -0,0 +1,77 @@
+package edu.westga.cs6312.sorting.model;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * This class will serve as a utility class manage timing of the sort methods.
+ * 
+ * @author J. Allen Burton
+ * @version Mar 21, 2020
+ *
+ */
+public class SortTimer {
+	private Instant startTime;
+	private Instant endTime;
+	private boolean isRunning;
+
+	/**
+	 * Constructor for SortTimer objects.
+	 *
+	 * @precondition none
+	 *
+	 * @postcondition new SortTimer object created with isRunning set to false
+	 */
+	public SortTimer() {
+		isRunning = false;
+	}
+
+	/**
+	 * This method starts the timer by recording the current time (from system
+	 * clock) as the start time and setting the isRunning status to true.
+	 *
+	 * @precondition isRunning == false
+	 *
+	 * @postcondition startTime is set to current time and isRunning is set to true
+	 */
+	public void startTimer() {
+		if (isRunning == false) {
+			this.startTime = Instant.now();
+			this.isRunning = true;
+		}
+	}
+
+	/**
+	 * This method stops the timer by recording the current time (from system clock)
+	 * as the end time and setting the isRunning status to false.
+	 *
+	 * @precondition isRunning == true
+	 *
+	 * @postcondition endTime is set to current time and isRunning status is set to
+	 *                false
+	 */
+	public void stopTimer() {
+		if (isRunning == true) {
+			this.endTime = Instant.now();
+			this.isRunning = false;
+		}
+	}
+
+	/**
+	 * This method computes the elapsed time from when the SortTimer object was
+	 * started and stopped measured in milliseconds.
+	 * 
+	 * @return	the number of milliseconds between the start time and end time in the current object
+	 *
+	 * @precondition	endTime must be set (which in-turn means startTime is set) && isRunning == false
+	 *
+	 * @postcondition	no change to object
+	 */
+	public long getElapsedTime() {
+		long elapsedTime = 0;
+		if (isRunning == false && this.endTime != null) {
+			elapsedTime = ChronoUnit.MILLIS.between(this.startTime, this.endTime);
+		}
+		return elapsedTime;
+	}
+}
